d41cdcd15665eea21ad339dc3cd7635f4c830060
Emin_Shabanli <shabanliemin17.09@gmail.com>
2026-04-08 23:06:04 +0400

    логические операции

---
+++ fintech.java
@@ -0,0 +1,49 @@
+
+//import java.util.Scanner;
+//import java.util.Scanner;
+import java.util.Scanner;
+public class fintech {
+    public static void main(String[] args) {
+
+       //System.out.print("большой");
+        //System.out.print("хуй");
+
+
+       //System.out.println(3+3);
+       //System.out.println(3+3);
+
+       //byte variabl1 = 31;
+       //System.out.println(variabl1);
+
+        //char gg = '=';
+        //System.out.println(gg);
+
+        //String хуй; 
+        //хуй = "При проуправленчиская дейтельность";
+
+        //System.out.println(хуй);
+        //System.out.println("Write here somethink");
+        //Scanner kuk = new Scanner(System.in);  
+       //String name =  kuk.nextLine();
+       //System.out.println(name); 
+
+       //ешё разок
+       //System.out.print("Please write somethink: ");
+        //Scanner сканить = new Scanner(System.in);
+        //boolean ansver1 = сканить.nextBoolean();
+        //System.out.println("Ansver 1: "+ansver1 );
+
+
+        //логические операции
+        System.out.print("Pleas chose a number:");
+        Scanner scan = new Scanner(System.in);
+        int an = scan.nextInt();
+        //options = 5,10,15,20
+        if (an == 5) { System.out.println("You choosed 5, have lucky one.");
+            
+        } else {
+            System.out.println("Error gijdulag");
+        }
+    } 
+
+}
