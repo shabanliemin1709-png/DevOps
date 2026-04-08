49e048de9b4a18889602d44ea7e5762c1b5da911
Emin_Shabanli <shabanliemin17.09@gmail.com>
2026-04-08 21:15:03 +0400

    Продолжени обучения

--- Обучалка.java
+++ Обучалка.java
@@ -1,11 +1,23 @@
 public class Обучалка {
     public static void main(String[] args) {
-         
-        byte money = 127;
 
-        System.out.println(money);
-
+       //System.out.print("большой");
+        //System.out.print("хуй");
 
 
+       //System.out.println(3+3);
+       //System.out.println(3+3);
+
+       //byte variabl1 = 31;
+       //System.out.println(variabl1);
+
+        //char gg = '=';
+        //System.out.println(gg);
+
+        String хуй; 
+        хуй = "При проуправленчиская дейтельность";
+
+        System.out.println(хуй);
     }
+
 }
