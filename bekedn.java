bd0ae1a8831e2a1dea3d31ad39310a55b79748ae
Emin_Shabanli <shabanliemin17.09@gmail.com>
2026-04-13 21:23:49 +0400

    дабиваю иф в ифе

---
+++ bekedn.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class bekedn {
+    public static void main(String[] args) {
+        Scanner ask = new Scanner(System.in);
+        System.out.println("What up?");
+        String uns1 = ask.nextLine();
+        if (uns1.equalsIgnoreCase("good")) {
+        System.out.println("do you need any think?");
+        String uns2 = ask.nextLine();
+        if (uns2.equalsIgnoreCase("yes")) {
+        System.out.println("Only Og krash, hoy many do you need?");
+        int uns3 = ask.nextInt();
+        int bill = uns3*70;
+        System.out.println("That'll " + bill);
+ 
+        }  System.out.println("I am lil bit busy, see you lettr bro");
+        } System.out.println("see u letter");
+    }
+}
--- fintech.java
+++ fintech.java
@@ -2,130 +2,25 @@
 //import java.util.Scanner;
 //import java.util.Scanner;
 //import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.Scanner;
 public class fintech {
     public static void main(String[] args) {
 
-       //System.out.print("большой");
-        //System.out.print("хуй");
+       Scanner get_data = new Scanner(System.in);
+    System.out.println("Добро пожаловать в баздых, вы месный?(yes / no)");
+    String local = get_data.nextLine();
+if (local.equalsIgnoreCase("yes")) {
+System.out.println("Сколько лет?");
+int age = get_data.nextInt();
+if (age >= 18) {
+    System.out.println("Деньги есть? а именно сколько");
+  int money = get_data.nextInt();
+  if (money >= 100) {
+System.out.println("проходи");    
+  } else System.out.println("Сорян не поебёшся :(, приходи когда будут деньги.");
+} else System.out.println("Сорян не поебёшся :(, не достаточно взрослый.");
+}else System.out.println("Сорян, не поебёшся :(, нет документов.");
 
+}}    
 
-       //System.out.println(3+3);
-       //System.out.println(3+3);
-
-       //byte variabl1 = 31;
-       //System.out.println(variabl1);
-
-        //char gg = '=';
-        //System.out.println(gg);
-
-        //String хуй; 
-        //хуй = "При проуправленчиская дейтельность";
-
-        //System.out.println(хуй);
-        //System.out.println("Write here somethink");
-        //Scanner kuk = new Scanner(System.in);  
-       //String name =  kuk.nextLine();
-       //System.out.println(name); 
-
-       //ешё разок
-       //System.out.print("Please write somethink: ");
-        //Scanner сканить = new Scanner(System.in);
-        //boolean ansver1 = сканить.nextBoolean();
-        //System.out.println("Ansver 1: "+ansver1 );
-
-
-        //логические операции
-        //System.out.print("Pleas chose a number, we have 5,10,15,20:");
-        //Scanner scan = new Scanner(System.in);
-        //System.out.println("Ведите логи:");
-        //String an1 = scan.nextLine();
-        //System.out.println("Ведите пароль:");
-        //String an2 = scan.nextLine();
-        ////options = 5,10,15,20
-        //if (an1 == 5 && an2 ==10 ) { System.out.println("You choosed first end");
-        //} else if (an1 == 20 && an2 ==40 ) { System.out.println("You choosed secend end");
-        //} 
-     
-        
-
-
-         //else if (an == 10) { System.out.println("You choosed 10, have lucky one.cffggt");
-            
-       // }else if (an == 15) { System.out.println("You choosed 15, have lucky one.");
-            
-       // }else if (an == 20) { System.out.println("You choosed 20, have lucky one.");
-            
-
-
-        //if (an1.equals("shabanliemin17.09") && an2.equals("emishAnin17.09")){ 
-         //   System.out.println("maaaa niga");
-            
-        //}
-
-        //else {
-        //    System.out.println("Error gijdulag");
-        //}
-
-        //Scanner suka = new Scanner(System.in);
-        //System.out.print("chose option:");
-        //int answer = suka.nextInt();
-
-
-
-        //switch (answer) {
-            //case 1:
-            //    System.out.println("first option");
-            //    break;
-
-            //case 2:
-              //  System.out.println("u choosed second option");
-                //break;
-
-            //case 3:
-              //  System.out.println("u choosed third option");
-             //   break;
-           
-            //default:
-               // System.out.println("Nothink");
-              //  break;
-
-        
-
-            
-//int x = 5;
-//int y = 6;
-
-//System.out.println("The sum is " + x + y);   // Prints: The sum is 56
-//System.out.println("The sum is " + (x + y)); // Prints: The sum is 11
-
-
-
-//int day = 8;
-//String answer = (day ==1 ) ? "work" : "else"; 
-//System.out.println(answer);
-
-//цель написать код котрый проверяет граждансво и возорст для входа в клуб. 
-
-Scanner ska = new Scanner(System.in);
-
-System.out.println("Hi, are you citizen? (yes or no)");
-String isCitizen = ska.nextLine();
-if (isCitizen.equalsIgnoreCase("yes")) {
-
-  System.out.println("Hou old are you&");
-
-int  age = ska.nextInt();
-
-if (age >= 18) { System.out.println("Pleas get in.");
-  
-} else System.out.println("Not enough old.");
-  
-} else System.out.println("Sorry, you must bi citizen.");
-
-
-
-
-
-
-/* конец*/ }}    
